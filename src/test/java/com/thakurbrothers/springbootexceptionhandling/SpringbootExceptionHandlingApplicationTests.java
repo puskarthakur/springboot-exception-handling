ad655877a8fc6f8cc7ce19dc91eab6886953d705
puskarthakur <puskar1303@gmail.com>
2023-06-06 17:55:37 +0530

    spring boot eception handling

---
+++ src/test/java/com/thakurbrothers/springbootexceptionhandling/SpringbootExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thakurbrothers.springbootexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
